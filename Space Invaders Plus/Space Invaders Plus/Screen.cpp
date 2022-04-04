7e5611bc32b66e08326134b98de279b26ca944ab
Aaron Hamilton <aaronleon28@gmail.com>
2022-04-04 16:05:35 +0100

    Created Screens class

---
+++ Space Invaders Plus/Space Invaders Plus/Screen.cpp
@@ -0,0 +1,34 @@
+#include "Screen.h"
+
+void Screen::initialise(){}
+void Screen::addPanel(std::unique_ptr<UIPanel> p, ScreenManagerRemoteControl* smrc, std::shared_ptr<InputHandler> ih)
+{
+	ih->initInputHandler(smrc, p->getButtons(), &p->m_View, this);
+	m_Panel.push_back(move(p));
+	m_InputHandler.push_back(ih);
+}
+
+void Screen::handleInput(RenderWindow& window)
+{
+	Event e;
+	auto itr = m_InputHandler.begin();
+	auto end = m_InputHandler.end();
+	while (window.pollEvent(e))
+	{
+		for (itr; itr != end; ++itr)
+			(*itr)->handleInput(window, e);
+	}
+}
+
+void Screen::update(float FPS){}
+
+void Screen::draw(RenderWindow& window){
+	auto itr = m_Panel.begin();
+	auto end = m_Panel.end();
+
+	for (itr; itr != end; ++itr)
+	{
+		(*itr)->draw(window);
+	}
+}
+
--- Space Invaders Plus/Space Invaders Plus/Screen.h
+++ Space Invaders Plus/Space Invaders Plus/Screen.h
@@ -1,7 +1,27 @@
 #ifndef Screen_H
 #define Screen_H
 
-class Screen {
+#include <SFML/Graphics.hpp>
+#include<vector>
+#include "InputHandler.h"
+#include "UIPanel.h"
+#include "ScreenManagerRemoteControl.h"
 
+
+class InputHandler;
+class Screen {
+private:
+	std::vector<std::shared_ptr<InputHandler>> m_InputHandler;
+	std::vector<std::unique_ptr<UIPanel>> m_Panel;
+protected:
+	void addPanel(std::unique_ptr<UIPanel> p, ScreenManagerRemoteControl* smrc, std::shared_ptr<InputHandler> ih);
+public:
+	virtual void initialise();
+	virtual void update(float FPS);
+	virtual void draw(RenderWindow& window);
+
+	void handleInput(RenderWindow& window);
+
+	View m_View;
 };
 #endif Screen_H
